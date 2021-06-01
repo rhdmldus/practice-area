a25db559bc8a4e33b575559366c0dbab09991ca7
EuiyeonKo <rhdmldus@gmail.com>
2021-06-01 17:33:06 +0900

    Update index.js

--- accordion/index.js
+++ accordion/index.js
@@ -5,7 +5,6 @@ btn.forEach(function (el) {
 });
 function toggleAccordion(el){
     let targetText = el.currentTarget.nextElementSibling.classList;
-    let target = el.currentTarget;
     if(targetText.contains('on')) {
         targetText.remove('on')
     }
@@ -17,3 +16,46 @@ function toggleAccordion(el){
         targetText.add('on')
     }
 }
+
+// variables
+// var accordionBtn = document.querySelectorAll('.accordionTitle');
+// var allTexts = document.querySelectorAll('.text');
+// var accIcon = document.querySelectorAll('.accIcon');
+
+// event listener
+// accordionBtn.forEach(function (el) {
+//     el.addEventListener('click', toggleAccordion)
+// });
+
+// function
+
+// function toggleAccordion(el) {
+//     var targetText = el.currentTarget.nextElementSibling.classList;
+//     var targetAccIcon = el.currentTarget.children[0];
+//     var target = el.currentTarget;
+    
+//     if (targetText.contains('show')) {
+//         targetText.remove('show');
+//         targetAccIcon.classList.remove('anime');
+//         target.classList.remove('accordionTitleActive');
+//     } 
+//     else {
+//         accordionBtn.forEach(function (el) {
+//             el.classList.remove('accordionTitleActive');
+            
+//             allTexts.forEach(function (el) {
+//                 el.classList.remove('show');
+//             })
+            
+//             accIcon.forEach(function (el) {
+//                 el.classList.remove('anime');
+//             }) 
+            
+//         })
+        
+//         targetText.add('show');
+//         target.classList.add('accordionTitleActive');
+//         targetAccIcon.classList.add('anime');
+//     }  
+// }
+// https://wsss.tistory.com/1291
