601153d4663e1f4630bd835ba0a876ae2cd23902
EuiyeonKo <rhdmldus@gmail.com>
2021-06-02 11:13:36 +0900

    Update index.js

--- accordion/index.js
+++ accordion/index.js
@@ -1,22 +1,31 @@
 let btn = document.querySelectorAll('.btn_title');
 let content = document.querySelectorAll('.content_box')
+let icon = document.querySelectorAll('.icon')
 btn.forEach(function (el) {
     el.addEventListener('click', toggleAccordion)
 });
 function toggleAccordion(el){
     let targetText = el.currentTarget.nextElementSibling.classList;
+    let targetTitle = el.currentTarget.classList;
     let target = el.currentTarget;
     if(targetText.contains('on')) {
         targetText.remove('on')
+        targetTitle.remove('on')
     }
     else {
         btn.forEach(function(el) {
             console.log(el)
+            el.classList.remove('on')
             content.forEach(function(el){
                 el.classList.remove('on')
             })
+            // icon.forEach(function(el){
+            //     el.parentNode.classList.remove('on')
+            // })
+            
         })
         targetText.add('on')
+        targetTitle.add('on')
     }
 }
 
