6bbc54f87dcb98f69c6f55271256f4490a426af7
EuiyeonKo <rhdmldus@gmail.com>
2021-06-04 17:08:43 +0900

    Update jason.js

--- javascript/jason.js
+++ javascript/jason.js
@@ -1,1 +1,37 @@
-let jason = JSON.
+// 1.object to JASON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple','banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'cat',
+    color: 'white',
+    size: null,
+    birthDate : new Date(),
+    jump: () => {
+        console.log(`${this.name} can jump!`);
+    },
+};
+// json = JSON.stringify(rabbit)
+console.log(JSON.stringify(rabbit, ['name', 'size']))
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`)
+    return key === 'name' ? 'dog' : value;
+})
+console.log(json)
+
+console.clear()
+
+// JSON to Object
+// parseFloat(json)
+json = JSON.stringify(rabbit)
+console.log(json)
+const obj = JSON.parse(json, (key, value) => {
+    return key === 'birthDate' ? new Date(value) : value;
+})
+console.log(obj)
+console.log(rabbit.birthDate.getDate())
+console.log(obj.birthDate.getDate())
