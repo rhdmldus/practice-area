7a7d3582229e47a2993e9516c7a0ab7d983ccef6
EuiyeonKo <rhdmldus@gmail.com>
2021-06-10 10:28:21 +0900

    rename folder

--- learning-react/01-starting-setup/src/components/ExpenseItem.js
+++ learning-react/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import './ExpenseItem.css'
-
+import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
-const month = props.date.toLocaleString('en-US', { month : 'short'})
-const year = props.date.toLocaleString('en-US', { year : '2-digit'})
-const day = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
